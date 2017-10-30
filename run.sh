4a1272819803cd3018963c6f4510e3cc94f31b9b
kingkrausel <denis.golovin@rwth-aachen.de>
2017-10-30 15:25:51 +0100

    do not wait

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@
 ./config_and_start_mysql.sh
 ./config_apache.sh
 ./config_wordpress.sh
-#./wait-for-it.sh localhost:3306 -t 99
+./wait-for-it.sh localhost:3306 -t 99
 ./install-wp-tests.sh wordpress_test root '' localhost latest
 
 echo "=> Apache started..."
