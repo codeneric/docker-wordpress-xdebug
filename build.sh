fc4e3b8ccfe3269340785512d03be623c47f670c
Denis <denis@codeneric.com>
2018-07-15 20:44:11 +0200

    no-cache optional

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
 #docker rmi -f registry.codeneric.com:5000/wordpress-xdebug
+# docker build  --no-cache  -t codeneric/wordpress-xdebug:latest .
 docker build -t codeneric/wordpress-xdebug:latest .
